b55f4e9c966a3eff6d9af090880d66aad965d34a
Sarthak2477 <sarthakp8074@gmail.com>
2023-04-07 15:53:28 +0530

    modified:   EnergyBillingSystem.java

--- EnergyBillingSystem.java
+++ EnergyBillingSystem.java
@@ -1,28 +1,37 @@
 package Microproject;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import Microproject.*;
 
 public class EnergyBillingSystem {
     public static void main(String[] args) {
-        System.out.println("Enter Customer details: ");
+        ArrayList<Customer> customerList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Customer Name: ");
-        String name = sc.nextLine();
-        System.out.print("Customer Address: ");
-        String address = sc.nextLine();
-        System.out.print("Customer Connection Type: ");
-        String connectionType = sc.nextLine();
-        System.out.print("Customer Contact: ");
-        long contact = sc.nextLong();
-        System.out.print("Customer Meter Number: ");
-        int meterNum = sc.nextInt();
-        Customer customer = new Customer(name, address, connectionType, contact, meterNum);
+        Customer customer;
+        System.out.println("Enter 1. Create new Customer 2.Get Customer Bill");
+        int opt = sc.nextInt();
+        if (opt == 1) {
+            System.out.println("Enter Customer details: ");
+            System.out.print("Customer Name: ");
+            String name = sc.nextLine();
+            System.out.print("Customer Address: ");
+            String address = sc.nextLine();
+            System.out.print("Customer Connection Type: ");
+            String connectionType = sc.nextLine();
+            System.out.print("Customer Contact: ");
+            long contact = sc.nextLong();
+            System.out.print("Customer Meter Number: ");
+            int meterNum = sc.nextInt();
+            customer = new Customer(name, address, connectionType, contact, meterNum);
+            customerList.add(customer);
+        }
         System.out.print("Enter unit: ");
         Bill bill = new Bill(customer, sc.nextInt());
         bill.calculateBill();
         bill.getDueDate();
         System.out.println(bill);
-        
+
     }
 }
